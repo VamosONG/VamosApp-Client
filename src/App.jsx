6c6aaad43849fae336b73e2c1b87eeeee4141b6e
Fishlennon <jpaviles5@gmail.com>
2024-01-16 20:49:45 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,49 @@
+
+import { useState } from 'react'
+import './App.css'
+
+import Footer from './views/footer/footer';
+import Paginado from './components/paginado/paginadoComponent';
+import LoginForm from './views/Forms/Login/Login'
+import HomeComponent from './views/home/homeCompone/home';
+import NavBar from './components/navBar/NavBar'
+import About from './components/about/About'
+import DetailChofer from './views/detailChofer/DetailChofer';
+import Product from './components/product/Product';
+// Hooks
+import {Routes, Route} from 'react-router-dom'
+import SolicitudViajeForm from './views/Forms/SolicitudViaje/SolicitudViajeForm';
+import SolicitudesDeViajes from './components/solicitudes/solicitudesDeViajes';
+import Solicitud from './components/solicitudes/solicitud';
+import { Link } from 'react-router-dom';
+import ReserveComfirmed from './views/Reserve/ReserveConfirmed';
+import ReserveReject from './views/Reserve/ReserveReject';
+
+function App() {
+
+  return (
+    <>
+      <NavBar/>
+
+
+      <Routes>
+        <Route path='/home' element={<LoginForm/>}/>
+        <Route path= '/about' element={<About/>} />
+        <Route path= '/solicitarViaje' element={<SolicitudViajeForm/>} />
+        <Route path= '/solicitudesDeViajes' element={<SolicitudesDeViajes/>} />
+        <Route path='/detail' element={<DetailChofer/>}/>
+        <Route path='/solicitud' element={<Solicitud/>}/>
+        <Route path='/reserve/confirmed' element={<ReserveComfirmed/>}/>
+        <Route path='/reserve/rejected' element={<ReserveReject/>}/>
+
+
+      </Routes>
+      <HomeComponent/>
+      {/* <Paginado/> */}
+
+      <Footer/>
+    </>
+  )
+}
+
+export default App
